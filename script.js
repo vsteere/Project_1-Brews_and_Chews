b93d35a2d9015cc8ffa8181ef7f44d1bee5a8998
vsteere <vssteere@gmail.com>
2020-08-11 20:51:01 -0500

    open brew API fully operational

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ console.log("hello")
 $(document).ready(function () {
 
     //variables defining the six boxes where the brewery information will go
-    // var box1 = $("#box1");
-    // var box2 = $("#box2");
-    // var box3 = $("#box3");
-    // var box4 = $("#box4");
-    // var box5 = $("#box5");
-    // var box6 = $("#box6");
+    var box1 = $("#box1");
+    var box2 = $("#box2");
+    var box3 = $("#box3");
+    var box4 = $("#box4");
+    var box5 = $("#box5");
+    var box6 = $("#box6");
 
     //function and AJAX functioning as Designed
     function breweryByname() {
@@ -51,7 +51,7 @@ $(document).ready(function () {
 
             var brewWeb1 = response[0].website_url;
             var brewWeb1p = $("<p>");
-            brewWWeb1p.text(`Website: ${brewWeb1}`);
+            brewWeb1p.text(`Website: ${brewWeb1}`);
             box1.append(brewWeb1p);
 
             //box 2 data
@@ -77,7 +77,7 @@ $(document).ready(function () {
 
             var brewWeb2 = response[1].website_url;
             var brewWeb2p = $("<p>");
-            brewWWeb2p.text(`Website: ${brewWeb2}`);
+            brewWeb2p.text(`Website: ${brewWeb2}`);
             box2.append(brewWeb2p);
 
             //box 3 data
@@ -103,34 +103,86 @@ $(document).ready(function () {
 
             var brewWeb3 = response[2].website_url;
             var brewWeb3p = $("<p>");
-            brewWWeb3p.text(`Website: ${brewWeb3}`);
+            brewWeb3p.text(`Website: ${brewWeb3}`);
             box3.append(brewWeb3p);
 
             //box 4 data
             var brewName4 = response[3].name;
             var brewname4H = $("<h5>");
             brewname4H.text(`Name: ${brewName4}`);
-            box3.append(brewname3H);
+            box4.append(brewname4H);
 
-            var brewType3 = response[2].brewery_type;
-            var brewType3p = $("<p>");
-            brewType3p.text(`Type: ${brewType3}`)
-            box3.append(brewType3p);
+            var brewType4 = response[3].brewery_type;
+            var brewType4p = $("<p>");
+            brewType4p.text(`Type: ${brewType4}`)
+            box4.append(brewType4p);
 
-            var brewStreet3 = response[2].street;
-            var brewstreet3P = $("<p>");
-            brewstreet3P.text(`Address: ${brewStreet3}`);
-            box3.append(brewstreet3P);
+            var brewStreet4 = response[3].street;
+            var brewstreet4P = $("<p>");
+            brewstreet4P.text(`Address: ${brewStreet4}`);
+            box4.append(brewstreet4P);
 
-            var brewPhone3 = response[2].phone;
-            var brewPhone3p = $("<p>");
-            brewPhone3p.text(`Phone Number: ${brewPhone3}`);
-            box3.append(brewPhone3p);
+            var brewPhone4 = response[3].phone;
+            var brewPhone4p = $("<p>");
+            brewPhone4p.text(`Phone Number: ${brewPhone4}`);
+            box4.append(brewPhone4p);
 
-            var brewWeb3 = response[2].website_url;
-            var brewWeb3p = $("<p>");
-            brewWWeb3p.text(`Website: ${brewWeb3}`);
-            box3.append(brewWeb3p);
+            var brewWeb4 = response[3].website_url;
+            var brewWeb4p = $("<p>");
+            brewWeb4p.text(`Website: ${brewWeb4}`);
+            box4.append(brewWeb4p);
+
+            //box 5 data
+            var brewName5 = response[4].name;
+            var brewname5H = $("<h5>");
+            brewname5H.text(`Name: ${brewName5}`);
+            box5.append(brewname5H);
+
+            var brewType5 = response[4].brewery_type;
+            var brewType5p = $("<p>");
+            brewType5p.text(`Type: ${brewType5}`)
+            box5.append(brewType5p);
+
+            var brewStreet5 = response[4].street;
+            var brewstreet5P = $("<p>");
+            brewstreet5P.text(`Address: ${brewStreet5}`);
+            box5.append(brewstreet5P);
+
+            var brewPhone5 = response[4].phone;
+            var brewPhone5p = $("<p>");
+            brewPhone5p.text(`Phone Number: ${brewPhone5}`);
+            box5.append(brewPhone5p);
+
+            var brewWeb5 = response[4].website_url;
+            var brewWeb5p = $("<p>");
+            brewWeb5p.text(`Website: ${brewWeb5}`);
+            box5.append(brewWeb5p);
+
+             //box 6 data
+             var brewName6 = response[5].name;
+             var brewname6H = $("<h5>");
+             brewname6H.text(`Name: ${brewName6}`);
+             box6.append(brewname6H);
+ 
+             var brewType6 = response[5].brewery_type;
+             var brewType6p = $("<p>");
+             brewType6p.text(`Type: ${brewType6}`)
+             box5.append(brewType6p);
+ 
+             var brewStreet6 = response[5].street;
+             var brewstreet6P = $("<p>");
+             brewstreet6P.text(`Address: ${brewStreet6}`);
+             box5.append(brewstreet6P);
+ 
+             var brewPhone6 = response[5].phone;
+             var brewPhone6p = $("<p>");
+             brewPhone6p.text(`Phone Number: ${brewPhone6}`);
+             box6.append(brewPhone6p);
+ 
+             var brewWeb6 = response[5].website_url;
+             var brewWeb6p = $("<p>");
+             brewWeb6p.text(`Website: ${brewWeb6}`);
+             box6.append(brewWeb6p);
 
 
         });
